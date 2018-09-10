dec3894fbe842f4e63ffb63270b612d2777b883f
Jennifer Colna <jennifer.colna@gmail.com>
2018-09-10 11:46:58 -0500

    styles and deploying

--- src/components/app.js
+++ src/components/app.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import {connect} from 'react-redux';
 import {Route, withRouter} from 'react-router-dom';
-
+import './app.css';
 import HeaderBar from './header-bar';
 import LandingPage from './landing-page';
 import Dashboard from './dashboard';
@@ -40,7 +40,7 @@ export class App extends React.Component {
 
   render() {
     return (
-      <div className="app">
+      <div className="app row">
         <HeaderBar />
         <Route exact path="/" component={LandingPage} />
         <Route exact path="/dashboard" component={Dashboard} />
--- src/components/landing-page.js
+++ src/components/landing-page.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import {connect} from 'react-redux';
 import {Link, Redirect} from 'react-router-dom';
-
+import './landing-page.css';
 import LoginForm from './login-form';
 
 export function LandingPage(props) {
@@ -11,7 +11,7 @@ export function LandingPage(props) {
   }
 
   return (
-    <div className="home">
+    <div className="home col-2">
       <h2>Welcome to ¡Hablamos!</h2>
       <p>Looking to take your Spanish to the next level?  ¡Hablamos! uses scientifically proven learning techniques to help you learn Spanish in no time!</p>
       <LoginForm />
@@ -21,7 +21,7 @@ export function LandingPage(props) {
 }
 
 const mapStateToProps = state => ({
-  loggedIn: state.auth.currentUser !== null
+  loggedIn: state.auth.currentUser !== null,
 });
 
 export default connect(mapStateToProps)(LandingPage);
