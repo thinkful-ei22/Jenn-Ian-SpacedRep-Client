1e6c557dd5b4f1d56b8c4742fba095544cbc11f1
Jennifer Colna <jennifer.colna@gmail.com>
2018-09-10 10:44:35 -0500

    added app name

--- src/components/registration-page.js
+++ src/components/registration-page.js
@@ -12,7 +12,7 @@ export function RegistrationPage(props) {
   }
   return (
     <div className="home">
-      <h2>Register for Foo App</h2>
+      <h2>Register for ¡Hablamos!</h2>
       <RegistrationForm />
       <Link to="/">Login</Link>
     </div>
