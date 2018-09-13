a75bac8facfbf7b21f8d8f1383de8a327a02f275
Ian Beihl <ianbeihl@gmail.com>
2018-09-13 12:18:26 -0700

    cleaned up landing page and registration page

--- src/components/landing-page.js
+++ src/components/landing-page.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import {connect} from 'react-redux';
-import {Link, Redirect} from 'react-router-dom';
+import { connect } from 'react-redux';
+import { Link, Redirect } from 'react-router-dom';
 import './landing-page.css';
 import LoginForm from './login-form';
 
@@ -12,16 +12,21 @@ export function LandingPage(props) {
 
   return (
     <div className="home col-2">
-      <h2>¡Bienvenido!</h2>
-      <p>Looking to take your Spanish to the next level?  ¡Hablamos! uses scientifically proven learning techniques to help you learn Spanish in no time!</p>
-      <LoginForm />
-      <Link to="/register">Register</Link>
+      <h2 className="ui huge header">
+        <img className="ui image" src="https://th.seaicons.com/wp-content/uploads/2015/11/Cultures-Sombrero-icon.png" alt="hat"/>
+          <div className="content">
+          ¡Bienvenido!
+          </div>
+      </h2>
+        <p>Looking to take your Spanish to the next level?  ¡Hablamos! uses scientifically proven learning techniques to help you learn Spanish in no time!</p>
+        <LoginForm />
+        <Link to="/register">Register</Link>
     </div>
-  );
-}
-
+      );
+    }
+    
 const mapStateToProps = state => ({
-  loggedIn: state.auth.currentUser !== null,
-});
-
-export default connect(mapStateToProps)(LandingPage);
+        loggedIn: state.auth.currentUser !== null,
+    });
+    
+    export default connect(mapStateToProps)(LandingPage);
--- src/components/registration-page.js
+++ src/components/registration-page.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import {connect} from 'react-redux';
-import {Link, Redirect} from 'react-router-dom';
+import { connect } from 'react-redux';
+import { Link, Redirect } from 'react-router-dom';
 
 import RegistrationForm from './registration-form';
 
@@ -11,7 +11,15 @@ export function RegistrationPage(props) {
     return <Redirect to="/dashboard" />;
   }
   return (
-    <div className="home">
+    <div className="home col-2">
+      <h2 className="ui huge header">
+        <img className="ui image" src="https://th.seaicons.com/wp-content/uploads/2015/11/Cultures-Sombrero-icon.png" alt="hat" />
+        <div className="content">
+          ¡Bienvenido!
+          </div>
+      </h2>
+      <p>Looking to take your Spanish to the next level?  ¡Hablamos! uses scientifically proven learning
+        techniques to help you learn Spanish in no time!</p>
       <h2>Register for ¡Hablamos!</h2>
       <RegistrationForm />
       <Link to="/">Login</Link>
