326b8c1f0083f8edcc7a81ef8b922afeab43262b
Ian Beihl <ianbeihl@gmail.com>
2018-09-14 10:36:53 -0700

    added some components

--- src/components/dashboard.js
+++ src/components/dashboard.js
@@ -2,11 +2,12 @@ import React from 'react';
 import { connect } from 'react-redux';
 import requiresLogin from './requires-login';
 import { fetchQuestion } from '../actions/questions';
-// import { Link, Redirect } from 'react-router-dom';
-import { checkAnswer, clearFeedback } from '../actions/check-answer';
-import ResetBtn from './reset-btn';
+import { checkAnswer } from '../actions/check-answer';
 import './dashboard.css';
 import HeaderBar from './header-bar';
+import ResponseBoxes from './response-boxes';
+import Welcome from './welcome';
+
 
 export class Dashboard extends React.Component {
   constructor(props) {
@@ -18,19 +19,16 @@ export class Dashboard extends React.Component {
   componentDidMount() {
     this.props.dispatch(fetchQuestion(this.props.userId));
   }
-
-  nextQuestion() {
-    this.props.dispatch(fetchQuestion(this.props.userId));
-    this.props.dispatch(clearFeedback());
-  }
-
   handleAnswerSubmit(e) {
     e.preventDefault();
     const answerObj = {
       userAnswer: e.target.answer.value,
       currentQuestionSpanish: this.props.currentQuestion.spanish,
     };
-    e.target.answer.value = '';
+
+    this.setState({
+      searchBtnDisabled: true
+    })
     this.props.dispatch(checkAnswer(answerObj, this.props.userId));
   }
 
@@ -58,74 +56,28 @@ export class Dashboard extends React.Component {
     } else {
       spanishWord = this.props.currentQuestion.spanish;
     }
-    let sessionScore;
-    if (isNaN((this.props.correct / this.props.total) * 100)) {
-      sessionScore = <h3>Your Session Score is {`${0}%`}</h3>;
-    }
-    else if (!isNaN((this.props.correct / this.props.total) * 100)) {
-      sessionScore = <h3>Your Session Score is {this.props.correct} out of {this.props.total} or {`${Math.round((this.props.correct / this.props.total) * 100)}%`}</h3>;
-    }
-    let correctMessage;
-    let incorrectMessage;
+
     let answerInput = <input onChange={(e) => this.handleDisable(e)} type="text" name="answer" placeholder="your guess..." className="answer ui input"></input>;
-
-    if (this.props.feedback !== null && this.props.feedback.feedback === true) {
+    if (this.props.feedback !== null) {
       answerInput = '';
-      correctMessage =
-        <div className="ui green inverted segment" id="success">
-          <h3>¡Muy bien!</h3>
-          <p>The answer is {this.props.feedback.correctAnswer}.
-            <br></br>
-            On to the next one!
-          </p>
-          <button className="ui button large" onClick={() => this.nextQuestion()}>Next Word</button>
-        </div>;
-    } else if (this.props.feedback !== null && this.props.feedback.feedback === false) {
-      answerInput = '';
-      incorrectMessage =
-        <div className="ui red inverted segment" id="incorrect">
-          <h3>¡Ay!</h3>
-          <p>The answer is {this.props.feedback.correctAnswer}.
-            <br></br>
-            You'll get it next time!
-          </p>
-          <button className="ui button large" onClick={e => this.nextQuestion(e)}>Next Word</button>
-        </div>;
-
     }
     let submitBtn;
 
     if (this.props.feedback === null) {
       submitBtn = <button disabled={disabled} className="submit-answer ui button large" >Submit Answer</button>;
     }
-    let score;
-    if (this.props.currentUser !== null && this.props.currentUser.questionsAnswered !== 0 && this.props.total === 0) {
-      score = Math.floor(100 * (this.props.currentUser.questionsCorrect / this.props.currentUser.questionsAnswered));
-    } else if (this.props.currentUser !== null && this.props.currentUser.questionsAnswered === 0 && this.props.total === 0) {
-      score = 0;
-    } else {
-      score = Math.floor(100 * (this.props.overallCorrect / this.props.overallAnswered));
-    }
 
     return (
       <div>
         <HeaderBar />
-        <div className="dashboard row">
-          <div id="welcome" className="dashboard-name">
-            <h1 className="welcome">Welcome to ¡Hablamos! {this.props.name}</h1>
-            <h3>Your Overall Score is {score} %</h3>
-            {sessionScore}
-            <ResetBtn />
-          </div>
-        </div>
+        <Welcome />
         {/* <div className="ui horizontal segments"> */}
         <div id="segment" className="word-display col-3 answering ui raised segment">
           <h2 className="spanish-word">{spanishWord}</h2>
           <form onSubmit={(e) => this.handleAnswerSubmit(e)}>
             {answerInput}
             {submitBtn}
-            {incorrectMessage}
-            {correctMessage}
+            <ResponseBoxes/>
           </form>
         </div>
       </div>
@@ -137,15 +89,9 @@ export class Dashboard extends React.Component {
 const mapStateToProps = state => {
   return {
     currentQuestion: state.questions.currentQuestion,
-    username: state.auth.currentUser.username,
     currentUser: state.auth.currentUser,
     userId: state.auth.currentUser._id,
-    name: state.auth.currentUser.firstName,
     feedback: state.checkAnswer.feedback,
-    total: state.checkAnswer.totalAnswered,
-    correct: state.checkAnswer.totalCorrect,
-    overallAnswered: state.checkAnswer.overallAnswered,
-    overallCorrect: state.checkAnswer.overallCorrect
   };
 };
 
