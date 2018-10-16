986df154225994e2df8d0a5dcc002e9424df4407
Jennifer Colna <jennifer.colna@gmail.com>
2018-10-16 13:24:26 -0500

    error overlap solved

--- src/components/input.js
+++ src/components/input.js
@@ -12,35 +12,53 @@ export default class Input extends React.Component {
     let error;
     if (this.props.meta.touched && this.props.meta.error) {
       error = <div className="form-error">{this.props.meta.error}</div>;
-    }
-    else{
-      error = <div className="form-error"></div>;
-    }
-
-    let warning;
-    if (this.props.meta.touched && this.props.meta.warning) {
-      warning = (
-        <div className="form-warning">{this.props.meta.warning}</div>
+      return(
+        <div className="form-input">
+          <label className="form-label" htmlFor={this.props.input.name}>
+            {this.props.label}
+          </label>
+          {error}
+          <input className="form-input"
+            {...this.props.input}
+            id={this.props.input.name}
+            type={this.props.type}
+            ref={input => (this.input = input)}
+          />
+        </div>
       );
     }
-    else{
-      warning = <div className='form-warning'></div>;
-    }
-
-    return (
-      <div className="form-input">
-        <label className="form-label" htmlFor={this.props.input.name}>
-          {this.props.label}
-          {error}
+    let warning;
+    if (this.props.meta.touched && this.props.meta.warning) {
+      warning = 
+        <div className="form-warning">{this.props.meta.warning}</div>;
+      return (
+        <div className="form-input">
+          <label className="form-label" htmlFor={this.props.input.name}>
+            {this.props.label}
+          </label>
           {warning}
-        </label>
-        <input className="form-input"
-          {...this.props.input}
-          id={this.props.input.name}
-          type={this.props.type}
-          ref={input => (this.input = input)}
-        />
-      </div>
-    );
+          <input className="form-input"
+            {...this.props.input}
+            id={this.props.input.name}
+            type={this.props.type}
+            ref={input => (this.input = input)}
+          />
+        </div>
+      );
+    }
+    else
+      return (
+        <div className="form-input">
+          <label className="form-label" htmlFor={this.props.input.name}>
+            {this.props.label}
+          </label>
+          <input className="form-input"
+            {...this.props.input}
+            id={this.props.input.name}
+            type={this.props.type}
+            ref={input => (this.input = input)}
+          />
+        </div>
+      );
   }
 }
